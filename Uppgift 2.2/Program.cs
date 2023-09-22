da345240127a20fb2d5d4662411ac9b224d329d4
Pingvu <nhlpool27@gmail.com>
2023-09-22 15:02:05 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace School_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade du Elin? Svara i meter.");
+            string ElinLängd = Console.ReadLine();
+            Console.WriteLine("Okej, hur långt hoppade du då Alma? Svara i meter.");
+            string AlmaLängd = Console.ReadLine();
+            int Elin = int.Parse(ElinLängd);
+            int Alma = int.Parse(AlmaLängd);
+            int Resulatat = Elin - Alma;
+            Console.WriteLine(Resulatat + " meter, är längd skillnad mellan Alma och Elin");
+        }
+    }
+}
